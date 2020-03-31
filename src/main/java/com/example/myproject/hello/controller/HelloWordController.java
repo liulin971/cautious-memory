54340db48707eaa8090c2355417adc9d52dcde13
liulin971 <13452779920@163.com>
2020-03-31 15:57:01 +0830

    测试idea提交代码

---
+++ src/main/java/com/example/myproject/hello/controller/HelloWordController.java
@@ -0,0 +1,11 @@
+package com.example.myproject.hello.controller;
+
+/**
+ * @author :liulin
+ * @version 1.0
+ * @ClassName: HelloWordController
+ * @description: TODO
+ * @date : 2020/3/31 15:51
+ */
+public class HelloWordController {
+}
